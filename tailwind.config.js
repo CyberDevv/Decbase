adadedfb0dfc3283d9fb9f013460e2f86087ff29
CyberDevv <tocyberdevv@gmail.com>
2021-03-26 21:39:19 +0100

    New Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'browner': '#bb967e',
         'brown': '#CAA892'
+      },
+      borderRadius: {
+        'large': '10rem'
+      },
+      spacing: {
+        '112': '28rem',
+        '128': '32rem',
+        '144': '36rem'
+      },
+      borderWidth: {
+        '3': '3px'
       }
     },
   },
